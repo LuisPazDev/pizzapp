22341dc2e56f565187663165293aff11e92c15ec
Luispaz86 <lupaz.dwfsc10@ucamp.utel.edu.mx>
2022-12-02 23:06:17 -0600

    updating

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,12 +1,20 @@
-import { Carousel } from './Carousel';
 import '../components/Menu.css';
+import menu1 from '../assets/menu1.png';
+import menu2 from '../assets/menu2.png';
+
+
 
 export const Menu = () => {
 
   return(
 
   <div className='container-menu'>
-      <Carousel />
+    <div>
+      <img className='img-menu2' src={menu1} alt="menu" />
+    </div>
+    <div>
+      <img className='img-menu1' src={menu2} alt="menu2" />
+    </div>
   </div>
   )
 };
