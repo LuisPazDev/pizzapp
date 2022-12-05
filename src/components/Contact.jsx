55d548e10feeeec0820682d2e8d60aeb58fdd1e2
Luispaz86 <lupaz.dwfsc10@ucamp.utel.edu.mx>
2022-12-05 13:00:57 -0600

    updating Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,28 @@
-export const Reserve = () => {
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
-
+export const Contact = () => {
+  
   return(
+    <Form>
+    <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Label>Email address</Form.Label>
+      <Form.Control type="email" placeholder="Enter email" />
+      <Form.Text className="text-muted">
+        We'll never share your email with anyone else.
+      </Form.Text>
+    </Form.Group>
 
-    <h1> Contact </h1>
-
-
+    <Form.Group className="mb-3">
+      <Form.Label>Comment</Form.Label>
+      <Form.Control type="text" placeholder="Comment" />
+    </Form.Group>
+    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+      <Form.Check type="checkbox" label="Check me out" />
+    </Form.Group>
+    <Button variant="danger" type="submit">
+      Send
+    </Button>
+  </Form>
   )
 };
