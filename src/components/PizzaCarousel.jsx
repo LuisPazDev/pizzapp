3fc6f1139843cc6aa7abfabc14751605f59d8a81
Luis <luispazdev@gmail.com>
2023-07-03 11:02:40 -0500

    updating pizza pics

--- src/components/PizzaCarousel.jsx
+++ src/components/PizzaCarousel.jsx
@@ -9,21 +9,12 @@ export const PizzaCarousel = () => {
     <Carousel>
       <Carousel.Item interval={1000}>
         <img className="d-block w-100" src={pepperoniPizza} alt="First slide" />
-        <Carousel.Caption>
-          <h5>
-            TRY OUR{" "}
-            <Badge bg="dark">
-              <h4>
-                <strong>NEW</strong>
-              </h4>
-            </Badge>
-          </h5>
-          <Badge bg="light" className="text-dark">
-            <h4>
-              <strong>PEPPERONI PIZZA</strong>
-            </h4>
-          </Badge>
-          <Button className="mt-3">ORDER NOW</Button>
+        <Carousel.Caption className="text-end">
+          <Button className="mt-3">
+            <strong>
+              <i>ORDER NOW</i>
+            </strong>
+          </Button>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item interval={500}>
@@ -33,39 +24,21 @@ export const PizzaCarousel = () => {
           alt="Second slide"
         />
         <Carousel.Caption>
-          <h5>
-            HOUSE{" "}
-            <Badge bg="dark">
-              <h4>
-                <strong>SPECIAL</strong>
-              </h4>
-            </Badge>
-          </h5>
-          <Badge bg="light" className="text-dark">
-            <h4>
-              <strong>PRIMAVERA PIZZA</strong>
-            </h4>
-          </Badge>
-          <Button className="mt-3">ORDER NOW</Button>
+          <Button className="mt-3">
+            <strong>
+              <i>ORDER NOW</i>
+            </strong>
+          </Button>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img className="d-block w-100" src={pomodoroPizza} alt="Third slide" />
         <Carousel.Caption>
-          <h5>
-            TRY OUR{" "}
-            <Badge bg="dark">
-              <h4>
-                <strong>NEW</strong>
-              </h4>
-            </Badge>
-          </h5>
-          <Badge bg="light" className="text-dark">
-            <h4>
-              <strong>POMODORO PIZZA</strong>
-            </h4>
-          </Badge>
-          <Button className="mt-3">ORDER NOW</Button>
+          <Button className="mt-3">
+            <strong>
+              <i>ORDER NOW</i>
+            </strong>
+          </Button>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
