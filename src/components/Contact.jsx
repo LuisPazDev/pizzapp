7bc233bfb92f09620705c8ba80a34950ff23a86e
Luis <luispazdev@gmail.com>
2023-07-15 17:03:25 -0500

    updating new toogle color

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,11 +33,20 @@ export function Contact() {
   };
 
   return (
-    <Card className="mt-5 ms-auto me-auto p-4" style={{ width: "22rem" }}>
+    <Card
+      className="mt-5 ms-auto me-auto p-4 b-2 b-dark"
+      style={{ width: "24rem" }}
+    >
       <h2 className="mt-4 mb-5">Contact Us</h2>
       <Form className="mt-auto mb-auto" id="form" onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicName">
-          <Form.Label>Name</Form.Label>
+          <Form.Label>
+            <h5>
+              <strong>
+                <i>NAME</i>
+              </strong>
+            </h5>
+          </Form.Label>
           <Form.Control
             onChange={handleChange}
             name="name"
@@ -47,7 +56,13 @@ export function Contact() {
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email</Form.Label>
+          <Form.Label>
+            <h5>
+              <strong>
+                <i>EMAIL</i>
+              </strong>
+            </h5>
+          </Form.Label>
           <Form.Control
             onChange={handleChange}
             name="email"
@@ -57,18 +72,24 @@ export function Contact() {
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicDescription">
-          <Form.Label>Comment</Form.Label>
+          <Form.Label>
+            <h5>
+              <strong>
+                <i>MESSAGE</i>
+              </strong>
+            </h5>
+          </Form.Label>
           <Form.Control
             onChange={handleChange}
             name="comment"
             className="input-comment"
             type="text"
-            placeholder="Leave a Comment"
+            placeholder="Leave a Message here"
           />
         </Form.Group>
 
         <div className="text-center">
-          <Button className="mt-4" size="lg" type="submit">
+          <Button variant="dark" className="mt-4" size="lg" type="submit">
             Send
           </Button>
         </div>
