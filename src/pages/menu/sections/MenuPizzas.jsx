4a2694dc4fae839d6704ddc1e0d686b64c7ccdf2
Luis <luispazdev@gmail.com>
2023-07-25 10:55:24 -0500

    updating styles in menuPizza.jsx

--- src/pages/menu/sections/MenuPizzas.jsx
+++ src/pages/menu/sections/MenuPizzas.jsx
@@ -1,10 +1,7 @@
 import { useState, useEffect } from "react";
 import { db } from "../../../components/Firebase";
 import { collection, onSnapshot, query } from "firebase/firestore";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
+import { Card, Button, Badge, Row, Col } from "react-bootstrap";
 
 export const MenuPizzas = () => {
   const [list, setList] = useState([]);
@@ -22,18 +19,40 @@ export const MenuPizzas = () => {
   }, []);
 
   return (
-    <>
-      <div>
-        <h1 className="text-center mt-5"> Pizzas </h1>
-      </div>
-      <Row xs={1} md={2} lg={3} className="g-4 ">
+    <Row>
+      <Row>
+        <div className="text-center mt-4">
+          <h3>
+            <Badge pill bg="dark">
+              <strong>
+                <i>PIZZAS</i>
+              </strong>
+            </Badge>
+          </h3>
+          <p>
+            <i>
+              Choose your favorite pizza
+              <br />
+              and we will prepare it for you with the best ingredients.
+            </i>
+          </p>
+        </div>
+      </Row>
+
+      <Row xs={1} md={3} lg={3} className="g-4 ">
         {list.map((item) => (
           <Col>
             <Card
+              border="dark"
               style={{ width: "14rem" }}
-              className="text-center ms-auto me-auto"
+              className="text-center ms-auto me-auto rounded-start"
             >
-              <Card.Img variant="top" src={item.data.img} />
+              <Card.Img
+                className="img-fluid"
+                variant="top"
+                src={item.data.img}
+              />
+              <hr />
               <Card.Body>
                 <Card.Title>{item.data.name}</Card.Title>
                 <Card.Text>
@@ -41,12 +60,16 @@ export const MenuPizzas = () => {
 
                   <p> {item.data.price}</p>
                 </Card.Text>
-                <Button> See Ingredients </Button>
+                <Button variant="dark">
+                  <strong>
+                    <i>ORDER</i>
+                  </strong>
+                </Button>
               </Card.Body>
             </Card>
           </Col>
         ))}
       </Row>
-    </>
+    </Row>
   );
 };
