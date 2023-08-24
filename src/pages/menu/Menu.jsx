07582803f816826199287ca4f0a480a731a219f7
Luis <luispazdev@gmail.com>
2023-08-24 11:19:15 -0500

    adding new imgs to menu.jsx

--- src/pages/menu/Menu.jsx
+++ src/pages/menu/Menu.jsx
@@ -2,10 +2,6 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Button, Row, Col, Badge, Container, Image } from "react-bootstrap";
 
-import menupizza from "./assets/menupizzas.png";
-import menudrinhs from "./assets/menudrinks.png";
-import menudesserts from "./assets/menudesserts.png";
-
 export const Menu = () => {
   // Function to handle button hover
   const [hoverIndex, setHoverIndex] = useState(-1);
@@ -18,16 +14,34 @@ export const Menu = () => {
   };
 
   return (
-    <Container fluid>
-      <Row className="justify-content-center">
-        <Col xs={12} className="mb-4">
-          <Image src={menupizza} alt="Menu Pizzas" />
+    <Container fluid className="p-4">
+      <Row>
+        <Col
+          xs={12}
+          className="d-flex flex-column align-items-center justify-content-center"
+        >
+          <Image
+            src="https://res.cloudinary.com/dxctvkec9/image/upload/v1692889790/pizzaslide_lowaps.png"
+            alt="Menu Pizzas"
+          />
         </Col>
-        <Col xs={12} className="mb-4">
-          <Image src={menudrinhs} alt="Menu Drinks" />
+        <Col
+          xs={12}
+          className="d-flex flex-column align-items-center justify-content-center"
+        >
+          <Image
+            src="https://res.cloudinary.com/dxctvkec9/image/upload/v1692889773/drinkslide_wkueyw.png"
+            alt="Menu Drinks"
+          />
         </Col>
-        <Col xs={12} className="mb-4">
-          <Image src={menudesserts} alt="Menu Desserts" />
+        <Col
+          xs={12}
+          className="d-flex flex-column align-items-center justify-content-center"
+        >
+          <Image
+            src="https://res.cloudinary.com/dxctvkec9/image/upload/v1692889797/dessertslide_nfjynh.png"
+            alt="Menu Desserts"
+          />
         </Col>
       </Row>
     </Container>
